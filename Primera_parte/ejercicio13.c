c188211b9645a4f599fe8318956cb3b14e390749
Daniel <herbonniered@gmail.com>
2023-06-14 21:05:16 -0300

    Enunciado

--- Primera_parte/ejercicio13.c
+++ Primera_parte/ejercicio13.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/* ---Enunciado---
+Escribir un programa que calcule el factorial de un número n! a partir de un entero
+positivo n:
+- si n = 0, n! = 1
+- si n > 0, n! = n ∗ ( n - 1) ∗ (n - 2) ∗ . . . . ∗ 1
+*/
+
 int main() {
 	int i, number, result;
 	result = 1;
