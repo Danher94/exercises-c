6f37899509402a5ceba63aa934e93e0070561983
Daniel <herbonniered@gmail.com>
2023-06-19 12:50:00 -0300

    Coreccion del ejercicio 8

--- Primera_parte/Ejercicio8.c
+++ Primera_parte/Ejercicio8.c
@@ -2,8 +2,7 @@
 
 /* ---Enunciado---
 Escriba un programa que dada una hora en formato (horas, minutos, segundos)
-indiqué cual será
-el tiempo dentro de un segundo.
+indiqué cual será el tiempo dentro de un segundo.
 */
 
 int main(){
@@ -12,19 +11,28 @@ int main(){
 	printf("Ingrese la hora: ");
 	scanf("%d", &hour);
 	
-	printf("Ingrese los minutos: ");
+	printf("Ingrese los minutos (menor a 60): ");
 	scanf("%d", &min);
 	
-	printf("Ingrese los segundos: ");
+	printf("Ingrese los segundos (menor a 60): ");
 	scanf("%d", &sec);
 	
-	hour *= 3600;
-	
-	min *= 60;
-	
-	sec += min + hour;
-	
-	printf("Los datos corresponden a: %d segundos", sec);
+	if (min >= 60 || sec >= 60){
+		printf("Numeros no admitidos");
+	}else {
+		if (sec == 59){
+			sec = 0;
+			min += 1;
+			if (min == 60){
+				min = 0;
+				hour += 1;	
+			}			
+		}else{
+			sec += 1;
+		}
+		
+		printf("La hora dada luego de un segundo es de %d:%d:%d", hour, min, sec);
+	}
 	
 	return 0;
 }
