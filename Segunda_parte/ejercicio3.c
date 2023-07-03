d02a0a83010e28b27379d10320754960b430c544
Daniel <herbonniered@gmail.com>
2023-07-03 10:50:38 -0300

    Correccion del ejercicio 3

--- Segunda_parte/ejercicio3.c
+++ Segunda_parte/ejercicio3.c
@@ -7,7 +7,7 @@ Escribir un programa que indique si dos strings son iguales
 
 int main(){
 	char str1[100], str2[100];
-	int i, j = 1;
+	int i, AreEqual = 1;
 
 	printf("Ingrese el primer texto: ");
 	fgets(str1, sizeof(str1), stdin);
@@ -18,17 +18,18 @@ int main(){
 	if (strlen(str1) == strlen(str2)){
 		for	(i = 0; i <= strlen(str1) - 1; i++){
 			if(str1[i] != str2[i]){
-				j = 0;
+				AreEqual = 0;
+				break;
 			}
 		}
 	}else{
-		j = 0;
+		AreEqual = 0;
 	}
 
-	if (j == 0){
-		printf("Los textos no son iguales");
-	}else{
+	if (AreEqual){
 		printf("Los textos son iguales");
+	}else{
+		printf("Los textos no son iguales");
 	}
 	
 	return 0;
